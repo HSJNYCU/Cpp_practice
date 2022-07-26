b04f850904359b340d96a9d215809e2c1edf92aa
HSJNYCU <hsj4nycu.ee10@nycu.edu.tw>
2022-07-26 20:34:20 +0800

    0726

--- SAMS/CH4/CH4/CH4.cpp
+++ SAMS/CH4/CH4/CH4.cpp
@@ -1,20 +1,45 @@
 ﻿// CH4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
 
-#include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+// P70 Dynamic Arrays Listing 4.4
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+	vector<int> dynArray(3); // dynamic array of int
+	dynArray[0] = 365;
+	dynArray[1] = -421;
+	dynArray[2] = 789;
+	cout << "Number of integers in array: " << dynArray.size() << endl;
+
+	cout << "Enter another element to insert" << endl;
+	int newValue = 0;
+	cin >> newValue;
+	dynArray.push_back(newValue);
+
+	cout << "Number of integers in array: " << dynArray.size() << endl;
+	cout << "Last element in array: ";
+	cout << dynArray[dynArray.size() - 1] << endl;
+	return 0;
 }
 
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+
+
+
+//// P69 3 dimension array
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int threeDim[3][3] = { {1,2,3}, {4,5,6},{7,8,9} };
+//	cout << "Row 0: " << threeDim[0][0] << " " << threeDim[0][1] << " " << threeDim[0][2] << endl;
+//
+//	return 0;
+//}
+
+
