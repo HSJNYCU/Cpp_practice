393a6ce02f625c5557b74c3178a3f4fc828b1ad7
HSJNYCU <hsj4nycu.ee10@nycu.edu.tw>
2022-07-28 19:40:02 +0800

    0728

---
+++ SAMS/CH5/CH5/CH5.cpp
@@ -0,0 +1,26 @@
+﻿// Listing 5.5
+
+//// Listing 5.4 Three way comparison operator <=>
+//#include<iostream>
+//#include<compare>
+//using namespace std;
+//
+//int main()
+//{
+//    int num1, num2;
+//    cout << "Enter two integers: " << endl;
+//    cin >> num1;
+//    cin >> num2;
+//
+//    auto resultofComparison = (num1 <=> num2);
+//
+//    if (resultofComparison < 0)
+//        cout << "num1 < num2" << endl;
+//    else if (resultofComparison > 0)
+//        cout << "num1 > num2" << endl;
+//    else
+//        cout << "num1 = num2" << endl;
+//
+//}
+
+
---
+++ SAMS/CH6/CH6/CH6.cpp
@@ -0,0 +1,86 @@
+﻿// P.136 LISTING 6.12 Using a Range-Based for Loop over Arrays and a std::string
+
+
+
+//// P.134 LISTING 6.11 Using a for Loop Without a Loop Expression to Repeat Calculations on User Request
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	for (char userSelection = 'm'; (userSelection != 'x');) {
+//        cout << "Enter two integers: " << endl;
+//        int num1, num2;
+//        cin >> num1;
+//        cin >> num2;
+//
+//        cout << num1 << "x" << num2 << " = " << num1 * num2 << endl;
+//        cout << num1 << "+" << num2 << " = " << num1 + num2 << endl;
+//
+//        cout << "Press x to exit or any key for recalculate." << endl;
+//        cin >> userSelection;
+//	}
+//    return 0;
+//}
+
+
+
+
+
+
+//// P.132 LISTING 6.10 Using for Loops to Enter Elements in a Static Array and Display the Contents of the Array
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//	const int ARRAY_LENGTH = 5;
+//	int myNums[ARRAY_LENGTH] = { 0 };
+//
+//	cout << "Populate array of " << ARRAY_LENGTH << " integers " << endl;
+//
+//	for (int counter = 0; counter < ARRAY_LENGTH; counter++) {
+//		cout << "Enter an integer for element " << counter << ": ";
+//		cin >> myNums[counter];
+//	}
+//
+//	cout << "Displaying contents of the array" << endl;
+//	
+//	for (int counter = 0; counter < ARRAY_LENGTH; counter++) {
+//		cout << "Element " << counter << " = " << myNums[counter] << endl;
+//	}
+//
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+//// P.131 LISTING 6.9 Using do...while to Repeat Execution of a Block of Code
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    char userSelection = 'x';
+//    do
+//    {
+//        cout << "Enter two integers: " << endl;
+//        int num1, num2;
+//        cin >> num1;
+//        cin >> num2;
+//
+//        cout << num1 << "x" << num2 << " = " << num1 * num2 << endl;
+//        cout << num1 << "+" << num2 << " = " << num1 + num2 << endl;
+//
+//        cout << "Press x to exit or any key for recalculate." << endl;
+//        cin >> userSelection;
+//    } while (userSelection != 'x');
+//
+//    cout << "GoodBye!!" << endl;
+//    return 0;
+//}
+
